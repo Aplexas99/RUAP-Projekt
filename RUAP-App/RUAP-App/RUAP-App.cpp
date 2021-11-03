7f36ae1b5d95765d5c350f6003c32d782dbb12fa
Aplexas99 <toni_dzoic@hotmail.com>
2021-11-03 17:56:57 +0100

    Dodana Console App

---
+++ RUAP-App/RUAP-App/RUAP-App.cpp
@@ -0,0 +1,10 @@
+// RUAP-App.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Pozdrav iz aplikacije od Antonio Dzoic!\n";
+}
+
